230dc1de539c6b2fea7feb1017664966473f0783
nafisurrahmanfahim <nafisurr93@gmail.com>
2025-08-09 03:20:57 +0600

    demo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Dhaka, Banglaesh</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+880 1611720004</p>
                 </div>
               </div>
             </div>
